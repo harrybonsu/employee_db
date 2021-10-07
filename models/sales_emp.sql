b821f5fc021fd28791204cfd74595bea8487b576
Harry Bonsu <harrybonsu@gmail.com>
2021-10-07 17:55:08 +0000

    updating model files

--- models/sales_emp.sql
+++ models/sales_emp.sql
@@ -2,10 +2,37 @@
     materialized="table"
 )}}
 
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from employees
-inner join dept_emp
-on employees.emp_no = dept_emp.emp_no
-inner join departments
-on dept_emp.dept_no = departments.dept_no
-where dept_name = 'Sales'
+with employees as (
+
+  select * from {{ ref('stg_employees')}}
+),
+
+dept_emp as (
+
+  select * from {{ ref('stg_dept_emp') }}
+),
+
+departments as (
+  
+  select * from {{ ref('stg_departments') }}
+),
+
+sales_employees as (
+
+  select
+    employees.employee_number,
+    employees.first_name,
+    employees.last_name,
+    departments.department_name
+
+  from employees
+
+  inner join dept_emp using (employee_number)
+
+  inner join departments using (department_number)
+
+  where department_name = 'Sales'
+
+)
+
+select * from sales_employees
